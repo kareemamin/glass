2885133eb55e4eec0182a6e7ff7a1aa01d494944
Kareem Amin <kareem@thesharedweb.com>
2013-02-24 23:22:10 -0500

    Initial commit for the glass website

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Glass::Application.config.secret_token = '430b641da8eb6f9ef9ce10db202a5fd4635fe4dba1664b8839ec0b49b474d2454fa717667391bd86c5800d6bf72c372f8913ab1450bd402586424d9c4b802831'
